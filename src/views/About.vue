88391fe1c525dd85b03975d6be41aa48c1a128d6
김대종/익스트라넷실 <daijong.kim@yanolja.com>
2021-03-24 10:54:27 +0900

    vue.js ver3 init

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <textarea id="about"></textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class About extends Vue {
+  msg!: string;
+}
+</script>
