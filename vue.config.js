15a71832c38028dbca40ac43439c878b0fb53d33
김대종/익스트라넷실 <daijong.kim@yanolja.com>
2021-03-30 19:25:48 +0900

    github pages 디렉토리 변경

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
     chainWebpack: config => {
         config.module.rules.delete('eslint');
     },
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/docs',
     outputDir: 'docs'
 }
